c7159e94e0f5bcabaef9491697aeb71abe08e89a
ollivilmi <olli.vilmi@gmail.com>
2018-09-13 13:32:21 +0300

    Added installation script, removed unnecessary boilerplate icons & service worker

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Starting installation..."
+npm install
+cd client
+npm install
+npm install --save react-redux
+cd ..
+npm i nodemon -g
+npm i yarn -g
+npm i concurrently -g
+echo "Installation successful"
